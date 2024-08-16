be72262639e9ef5df77b4acf4a4f5212fe90ad55
Kim Angelo Alvarez <kimangeloalvarez@gmail.com>
2024-08-16 14:46:31 +0800

    updates

--- src/redux/auth/authOperation.js
+++ src/redux/auth/authOperation.js
@@ -60,7 +60,7 @@ export const login = createAsyncThunk(
  */
 export const logout = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
   try {
-    await axios.post('/users/logout');
+    await axios.post('/auth/logout');
     // After a successful logout, remove the token from the HTTP header
     clearAuthHeader();
   } catch (error) {
---
+++ src/redux/selectors.js
@@ -0,0 +1,6 @@
+import { createSelector } from '@reduxjs/toolkit';
+
+// authSelector
+export const selectIsLoggedIn = state => state.auth.isLoggedIn;
+export const selectUser = state => state.auth.user;
+export const selectIsRefreshing = state => state.auth.isRefreshing;
